25e32b47d4bed8b5067b409fbb7058267d0468dd
unknown <kevin@excession.co>
2020-12-28 11:20:10 +0000

    first code block

---
+++ JsonJava/src/jsonToJava.java
@@ -0,0 +1,27 @@
+import java.sql.*;
+
+
+public class jsonToJava {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+Class.forName("com.mysql.cj.jdbc.Driver");
+Connection conn=null;
+conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","root");
+
+//object of the statement class will help us to execute queries
+Statement st=conn.createStatement();
+ResultSet rs=st.executeQuery("select * from CustomerInfo");
+while(rs.next()) //setting pointer to a particular row
+{
+	System.out.println(rs.getString(1));
+	System.out.println(rs.getString(2));
+	System.out.println(rs.getInt(3));
+	System.out.println(rs.getString(4));
+}
+conn.close();
+
+
+	}
+
+}

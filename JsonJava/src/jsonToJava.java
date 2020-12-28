5d2e5f83301219265c47058d8d85abcbe7d5cc4f
unknown <kevin@excession.co>
2020-12-28 11:39:07 +0000

    updated ignore file

--- JsonJava/src/jsonToJava.java
+++ JsonJava/src/jsonToJava.java
@@ -7,7 +7,7 @@ public class jsonToJava {
 		// TODO Auto-generated method stub
 Class.forName("com.mysql.cj.jdbc.Driver");
 Connection conn=null;
-conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","root");
+conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/Business",username,password);
 
 //object of the statement class will help us to execute queries
 Statement st=conn.createStatement();
